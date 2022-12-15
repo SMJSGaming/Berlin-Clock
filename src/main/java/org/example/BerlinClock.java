fdfa497a38a0ecafe4a86dea839fdcd06a8e55e1
SMJSGaming <sjoerd.t@live.nl>
2022-12-15 21:12:36 +0100

    Initial untested commit

---
+++ src/main/java/org/example/BerlinClock.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+/**
+ * @author Sjoerd Teijgeler
+ * @version 0.1.0-SNAPSHOT
+ * A class which converts a provided time into the berlin clock.
+ * The result will be printed to the terminal
+ */
+@Setter
+@AllArgsConstructor
+public class BerlinClock {
+
+    private Date date;
+}
